4ffbb6fbf5c66184c7832a81dfb523f02b9c00f3
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-19 15:36:20 -0400

    Add initial D3 charts that build from static data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 
 import './App.scss';
 
-import Chart from './Components/Chart/Chart';
+import ChartContainer from './Components/Chart/ChartContainer';
 import DashBoard from './Components/DashBoard/DashBoard';
 import { TeamMembers } from './Components/TeamMembers/TeamMembers';
 
@@ -10,7 +10,7 @@ const App = () => {
 	return(
 		<div className="app-container">
 			<h1 className="app-header">BitMex Trading Bot</h1>
-			<Chart />
+			<ChartContainer />
 			<DashBoard />
 			<TeamMembers />
 		</div>
